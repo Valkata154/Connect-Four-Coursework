58ca764b278262a9fb85b59191bbbb6e51d32e63
valkata154 <valkata154@gmail.com>
2020-02-22 14:47:50 +0000

    Created Menu

---
+++ coursework.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+void menu()
+{
+	printf("\n-----------------------\n");
+	printf("-- Connect Four Game --\n");
+	printf("-----------------------\n\n");
+	printf("MENU:\n\n");
+	printf("[1] START GAME\n");
+	printf("[2] RULES\n");
+	printf("[3] EXIT\n");
+}
+
+int main(void)
+{
+	menu();
+}
